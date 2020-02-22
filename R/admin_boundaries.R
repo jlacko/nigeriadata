a67c30b8c2202ebaf96ccbaa118c3d10a4c3ec1b
Jindra Lacko <jindra.lacko@gmail.com>
2020-02-22 21:35:26 +0100

    states as proxy until cert gets resolved

--- R/admin_boundaries.R
+++ R/admin_boundaries.R
@@ -9,19 +9,23 @@
 #' @importFrom magrittr %>%
 #' @importFrom sf st_as_sf
 #'
+#' @examples
+#'
+#' plot(admin_boundaries("state"), max.plot = 1)
+#'
 #'
 
 admin_boundaries <- function(type) {
-  translator <- c("ward_admin_boundaries" = "sv_wards",
-                  "lga_admin_boundaries" = "sv_local_government_areas",
-                  "state_admin_boundaries" = "sv_states")
+  translator <- c("ward" = "sv_wards",
+                  "lga" = "sv_local_government_areas",
+                  "state" = "sv_states")
 
   if(!type %in% names(translator)) stop("Unknown administrative area!")
 
 #  downloader(translator[type]) # commented out until certificate issue gets resolved
 
-  data <- read.csv("~/Downloads/sv_local_government_areas.csv", stringsAsFactors = F) %>%
-#    dplyr::select(-FID, -timestamp) %>%
+  data <- read.csv("~/Downloads/sv_states.csv", stringsAsFactors = F) %>%
+    dplyr::select(-"FID", -"timestamp") %>%
     sf::st_as_sf(wkt = "geom", crs = 4326)
 
   data
