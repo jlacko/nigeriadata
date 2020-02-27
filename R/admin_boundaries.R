333e761da2b421f20e897bb26dce2f8e391c07a6
Jindra Lacko <jindra.lacko@gmail.com>
2020-02-27 10:19:43 +0100

    docu-tuning

--- R/admin_boundaries.R
+++ R/admin_boundaries.R
@@ -1,10 +1,18 @@
 #' Administrative boundaries
 #'
-#' This is a function returning admin area boundaries
+#' Function returning local administrative area boundaries in {sf} package format.
 #'
-#' @param type one of
+#' Recognized administrative areas are:
+#'  \describe{
+#'   \item{state}{States of Nigeria.}
+#'   \item{lga}{Local Government Areas of Nigeria.}
+#'   \item{ward}{Wards of Nigeria.}
+#' }
 #'
-#' @return data frame
+#' @param type one of "ward", "lga" and "state"
+#' @return data frame in \code{sf} package format
+#' @source \url{https://www.ehealthafrica.org/}
+#'
 #' @export
 #'
 #' @examples
@@ -12,7 +20,7 @@
 #' states <- areas("state")
 #' plot(sf::st_geometry(states))
 #'
-#'
+
 
 areas <- function(type) {
 
